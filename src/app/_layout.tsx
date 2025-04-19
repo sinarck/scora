c2b7b31593e5da5093d6aa688289a0e941e40293
Aadi <83467783+sinarck@users.noreply.github.com>
2025-04-19 17:33:21 -0500

    feat: new `PostHog` integration

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,19 @@
 import tw from "@/lib/tw";
 import { Stack } from "expo-router";
+import { PostHogProvider } from "posthog-react-native";
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: tw`bg-background`,
-        headerTintColor: tw.color("text-primary"),
-        headerTitleStyle: tw`text-primary`,
-        contentStyle: tw`bg-background`,
-      }}
-    />
+    <PostHogProvider apiKey="phc_FJxD6bI5W5CdWT2IdZfl3m9OB2IU0sOP8U2z0lpZS4c">
+      <Stack
+        screenOptions={{
+          headerStyle: tw`bg-background`,
+          headerTintColor: tw.color("text-primary"),
+          headerTitleStyle: tw`text-primary`,
+          contentStyle: tw`bg-background`,
+        }}
+      />
+    </PostHogProvider>
   );
 }
 
