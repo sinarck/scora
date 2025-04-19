8934884cf6434c63892fbd41f9dd2b4fb77e4522
Aadi <83467783+sinarck@users.noreply.github.com>
2025-04-19 16:49:43 -0500

    chore: cleaner file layout

--- app.config.ts
+++ app.config.ts
@@ -7,11 +7,11 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   scheme: "acumen",
   version: "1.0.0",
   orientation: "portrait",
-  icon: "./assets/icon.png",
+  icon: "./assets/images/icon.png",
   userInterfaceStyle: "dark",
   newArchEnabled: true,
   splash: {
-    image: "./assets/splash.png",
+    image: "./assets/images/splash.png",
     resizeMode: "contain",
     backgroundColor: "#111119",
   },
@@ -22,13 +22,13 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   android: {
     adaptiveIcon: {
-      foregroundImage: "./assets/adaptive-icon.png",
+      foregroundImage: "./assets/images/adaptive-icon.png",
       backgroundColor: "#111119",
     },
     userInterfaceStyle: "dark",
   },
   web: {
-    favicon: "./assets/favicon.png",
+    favicon: "./assets/images/favicon.png",
   },
 });
 
