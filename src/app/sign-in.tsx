d3b97e15941a71d855297a7ec35f5f1d594ff67d
Aadi <83467783+sinarck@users.noreply.github.com>
2025-04-19 17:19:17 -0500

    chore: new `eslint` config

---
+++ src/app/sign-in.tsx
@@ -0,0 +1,11 @@
+import tw from "@/lib/tw";
+import { Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    <View>
+      <Text style={tw`text-2xl font-bold`}>Sign In</Text>
+    </View>
+  );
+}
+
