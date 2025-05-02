9c4d945327bbecc09b0bc7c50043377d0278ccf4
Aadi <83467783+sinarck@users.noreply.github.com>
2025-05-02 13:03:44 -0500

    fix: splash screen loading

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,12 +2,35 @@ import { queryClient } from "@/lib/client";
 import tw from "@/lib/tw";
 import { useReactQueryDevTools } from "@dev-plugins/react-query";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
 import { PostHogProvider } from "posthog-react-native";
+import { useEffect } from "react";
+
 import { View } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   useReactQueryDevTools(queryClient);
+
+  const [loaded, error] = useFonts({
+    "Satoshi-Black": require("../../assets/fonts/Satoshi-Black.otf"),
+    "Satoshi-Bold": require("../../assets/fonts/Satoshi-Bold.otf"),
+    "Satoshi-Medium": require("../../assets/fonts/Satoshi-Medium.otf"),
+    "Satoshi-Regular": require("../../assets/fonts/Satoshi-Regular.otf"),
+    "Satoshi-Light": require("../../assets/fonts/Satoshi-Light.otf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
 
   return (
     <QueryClientProvider client={queryClient}>
