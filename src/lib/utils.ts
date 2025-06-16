a920f769b2a2d3158eaca691810f21b41dbdbed3
Aadi <83467783+sinarck@users.noreply.github.com>
2025-06-16 18:33:03 -0500

    chore: first iteration of rewrite

--- src/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-import { StyleProp, ViewStyle } from "react-native";
-
-export function cn(
-  ...styles: (StyleProp<ViewStyle> | undefined)[]
-): StyleProp<ViewStyle> {
-  return styles.filter(Boolean) as StyleProp<ViewStyle>;
-}
-
